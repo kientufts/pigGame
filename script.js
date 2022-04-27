abcc15011f6c959c85a4f36bbbab1e29faf8baf3
Kien Le <letrungkien1991@gmail.com>
2022-04-27 13:47:54 -0700

    hidden dice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const currentScore1 = document.getElementById('score--0');
+const currentScore2 = document.getElementById('score--1');
+const diceImg = document.querySelector('.dice');
+
+currentScore1.textContent = 0;
+currentScore2.textContent = 0;
+diceImg.classList.add('hidden');
