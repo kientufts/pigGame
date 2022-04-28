2d27e21473618daf206e693dc031157a89adbab9
Kien Le <letrungkien1991@gmail.com>
2022-04-27 22:48:11 -0700

    switch active player

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player1El = document.querySelector('.player--0');
+const player2El = document.querySelector('.player--1');
 const score1El = document.getElementById('score--0');
 const score2El = document.getElementById('score--1');
 const currentScore1El = document.getElementById('current--0');
@@ -11,12 +13,18 @@ const btnRollDiceEl = document.querySelector('.btn--roll');
 const btnNewGameEl = document.querySelector('.btn--new');
 const btnHoldEl = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
-score1El.textContent = 0;
-score2El.textContent = 0;
+score1El.textContent = scores[0];
+score2El.textContent = scores[1];
 
 diceImgEl.classList.add('hidden');
+
+const setCurrentScore = function (aScore) {
+  document.getElementById(`current--${activePlayer}`).textContent = aScore;
+};
 
 btnRollDiceEl.addEventListener('click', function () {
   const dice = Math.trunc(6 * Math.random()) + 1;
@@ -26,9 +34,14 @@ btnRollDiceEl.addEventListener('click', function () {
 
   if (dice != 1) {
     currentScore += dice;
-    currentScore1El.textContent = currentScore; // FIXME: Add to correc current player
-  }
+    setCurrentScore(currentScore);
+  } else {
+    currentScore = 0;
+    setCurrentScore(currentScore);
 
-  //3. Check if rolled 1: if true, switch player
-  //4. If false, add dice number to the current score
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player1El.classList.toggle('player--active');
+    player2El.classList.toggle('player--active');
+  }
 });
